fede2cd08cfcfdea473cfbc8cae2ae0d3a4dccb0
Harsimransingh <harsimransingh.bindra@colorado.edu>
2018-12-08 16:46:14 -0700

    All parts integrated

--- f_project.py
+++ f_project.py
@@ -1,11 +1,7 @@
+#!/usr/bin/python
 
-'''
-References:
-    https://bradmontgomery.net/blog/sending-sms-messages-amazon-sns-and-python/
-
-'''
-
-
+#References:
+#    https://bradmontgomery.net/blog/sending-sms-messages-amazon-sns-and-python/
 
 import sys
 from PyQt5.QtCore import pyqtSlot
@@ -19,16 +15,28 @@ from PyQt5 import QtWidgets
 from time import sleep
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
+from time import gmtime, strftime, sleep
+#MQTT setup
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient 
+
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
+import time
+continue_reading = True
 
 dynamodb = boto3.resource('dynamodb')
 
-client = boto3.client(
-        "sns",
-        aws_access_key_id =
-        aws_secret_access_key = 
-        region_name=
-        )
+myclient = AWSIoTMQTTClient("harry")
+myclient.configureEndpoint("a3t3pe4wp4iz6-ats.iot.us-east-1.amazonaws.com", 8883)
+myclient.configureCredentials("/home/pi/Desktop/final-project/aws_new/AmazonRootCA1.pem", "/home/pi/Desktop/final-project/aws_new/818af5f799-private.pem.key", "/home/pi/Desktop/final-project/aws_new/818af5f799-certificate.pem.crt")
+myclient.configureAutoReconnectBackoffTime(1, 32, 20)
+myclient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myclient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myclient.configureConnectDisconnectTimeout(10)  # 10 sec
+myclient.configureMQTTOperationTimeout(5)  # 5 sec
 
+myclient.connect()
 
 global listA
 global listB
@@ -36,10 +44,13 @@ global slot_inputvalue1
 global slot_temp2
 global flag
 global test_flag
+global rfid_value
+global RFID_val
+username_login_flag = 0
+rfid_login_flag = 0
 slot_value = 0
 username = '' 
 passwordc = ''
-global RFID_val
 repeat_flag = 1
 check_flag = 0
 proj_flag = 0
@@ -47,6 +58,80 @@ login_flag = 1
 conf_flag = 0
 mobile_number = ""
 first_name = ""
+
+
+class initial_page(QtWidgets.QDialog):
+    def __init__(self, parent= None):
+        super(initial_page, self).__init__(parent)
+        loadUi ('initial.ui',self)
+        self.setWindowTitle('Options')
+        self.username_button.clicked.connect(self.username_buttonc)  
+        self.rfid_button.clicked.connect(self.rfid_buttonc)  
+        self.unlock_button.clicked.connect(self.unlock_buttonc) 
+
+    def username_buttonc(self):
+        global username_login_flag
+        self.accept()
+        username_login_flag = 1
+
+    def rfid_buttonc(self):
+        global rfid_login_flag
+        self.accept()
+        rfid_login_flag = 1
+
+    def unlock_buttonc(self):
+        self.accept()
+
+class Login_rfid(QtWidgets.QDialog):
+    def __init__(self, parent= None):
+        super(Login_rfid, self).__init__(parent)
+        loadUi ('login_rfid.ui',self)
+        self.setWindowTitle('Login!')
+        self.scan_button.clicked.connect(self.login_compare)  
+    def login_compare(self):
+        global test_flag
+        global slot_value
+        global rfid_value
+        global continue_reading
+        TAGRead = MFRC522.MFRC522()
+        while continue_reading:
+            # Scan for cards   
+            time.sleep(1)
+            (status,TagType) = TAGRead.MFRC522_Request(TAGRead.PICC_REQIDL)
+            # If a card is found
+            if status == TAGRead.MI_OK:
+                print ("Card detected")
+    
+            # Get the UID of the card
+            (status,uid) = TAGRead.MFRC522_Anticoll()
+            # If we have the UID, continue
+            if status == TAGRead.MI_OK:
+                rfid_value = str(uid[0])+str(uid[1])+str(uid[2])+str(uid[3])
+                print(rfid_value)
+                continue_reading = False
+        table1 = dynamodb.Table('users')
+        response = table1.scan(
+                FilterExpression=Attr('RFID_value').eq(rfid_value)
+                )
+        items = response['Items']
+        for i in items:
+            global username
+            global password
+            username = i['email_id']
+            password = i['last_name']
+            if i['RFID_value'] != rfid_value:
+                QtWidgets.QMessageBox.warning(self,'Error','Invalid Entry')
+                sys.exit(app.exec_())       
+            elif i['slot'] == '0' and i['RFID_value'] == rfid_value:
+                test_flag = 1
+                self.accept()
+                break
+            elif i['RFID_value'] == rfid_value:
+                slot_value = i['slot']
+                self.accept()
+                test_flag = 0
+                break
+        print(items)
 
 class Login(QtWidgets.QDialog):
     def __init__(self, parent= None):
@@ -83,39 +168,36 @@ class Login(QtWidgets.QDialog):
                 slot_value = i['slot']
                 self.accept()
                 test_flag = 0
-                repeat_flag = 1
                 check_flag = 1
                 break
         print(items)
-#        if (self.inputuser.text() == '111' and self.inputpswd.text() == '111'):
-#            self.accept()
-#        else:
-#            QtWidgets.QMessageBox.warning(self,'Error','Invalid Entry')
-#            sys.exit(app.exec_())
-
             
 class confirm(QDialog):
     def __init__(self):
+        global first_name
+
         super(confirm, self).__init__()
         loadUi ('Final_project_Config.ui',self)
         self.setWindowTitle('Confirmation!')
-        self.pushButton4.clicked.connect(self.on_pushButton4_clicked)
-        self.pushButton5.clicked.connect(self.on_pushButton5_clicked)
-        self.on_pushButton4_clicked()        
+        self.label_6.setText(first_name)
+        current_time = strftime("%H:%M:%S", gmtime())
+        self.label_8.setText(current_time)
+        self.pushButton4.clicked.connect(self.on_pushButton4c_clicked)
+        self.pushButton5.clicked.connect(self.on_pushButton5c_clicked)
     
     @pyqtSlot()
    
-    def on_pushButton4_clicked(self):
-        global login_flag
-        global repeat_flag
-        login_flag = 1
-        repeat_flag = 1
-    def on_pushButton5_clicked(self):
-        global login_flag
-        global repeat_flag
-        login_flag = 1
-        repeat_flag = 1
-
+    def on_pushButton4c_clicked(self):
+        global first_name
+        global slot_inputvalue1
+        global mobile_number
+        message_value = "Booking complete for" + first_name + " Slot value: " + str (slot_inputvalue1) + " and dock 1"
+        message = "{\"message_value\":\"" + message_value +"\",\"mobile_number\":\"" + mobile_number + "\"}"
+        myclient.publish("harry_policy_new",message, 0)
+        self.accept()
+    def on_pushButton5c_clicked(self):
+        self.accept()
+        
 class checkout(QDialog):
     def __init__(self):
         super(checkout, self).__init__()
@@ -127,10 +209,10 @@ class checkout(QDialog):
         self.checkout_username.setText(username)
         self.checkout_slot.setText(str(slot_value))
         self.checkout_dock.setText("1")
-        self.checkout_yes.clicked.connect(self.on_checkout_yes_clicked)
-        self.checkout_no.clicked.connect(self.on_checkout_no_clicked)
+        self.checkout_yes.clicked.connect(self.on_checkout_yesc_clicked)
+        self.checkout_no.clicked.connect(self.on_checkout_noc_clicked)
 
-    def on_checkout_yes_clicked(self): 
+    def on_checkout_yesc_clicked(self): 
         global login_flag
         global repeat_flag
         global username
@@ -182,27 +264,23 @@ class checkout(QDialog):
                     ':val1': '0'
                     }
                 )
-        client.publish(
-        PhoneNumber = mobile_number,
-        Message = "Check out complete"
-        )
+        message_value = "Checkout complete for" + first_name + " Slot value: " + str (slot_inputvalue1) + " and dock 1"
+        message = "{\"message_value\":\"" + message_value +"\",\"mobile_number\":\"" + mobile_number + "\"}"
+        myclient.publish("harry_policy_new",message, 0)
         login_flag = 1
         repeat_flag = 1
-    def on_checkout_no_clicked(self):
-        global login_flag
-        global repeat_flag
-        login_flag = 1
-        repeat_flag = 1
-      # sys.exit(app.exec_())
-
+        QtWidgets.QMessageBox.warning(self,'Checkout complete','Checkout complete!!!!!')
+        self.accept()
+    def on_checkout_noc_clicked(self):
+        self.accept()
 class project(QDialog):
     def __init__(self):
         super(project, self).__init__()
         loadUi ('Final_project.ui',self)
         self.setWindowTitle('Welcome!')
-        self.pushButton2.clicked.connect(self.on_pushButton2_clicked)
-        self.pushButton3.clicked.connect(self.on_pushButton3_clicked)
-        self.pushButton.clicked.connect(self.on_pushButton_clicked)
+        self.pushButton2.clicked.connect(self.on_pushButton2c_clicked)
+        self.pushButton3.clicked.connect(self.on_pushButton3c_clicked)
+        self.pushButton.clicked.connect(self.on_pushButtonc_clicked)
         table = dynamodb.Table('Availability')
         response = table.scan(
                 FilterExpression=Attr('booking').eq("available") & Attr('dock').eq(1)
@@ -227,14 +305,11 @@ class project(QDialog):
                 self.slot2.setText("A")
             elif i['slot'] == 3:
                 self.slot3.setText("A")
-       #slot_inputvalue2
-        #self.slot10.setText("0")
-        #slot_inputvalue1
         global slot_temp2
         global slot_inputvalue1            
     @pyqtSlot()            
                           
-    def on_pushButton_clicked(self):
+    def on_pushButtonc_clicked(self):
         global slot_inputvalue1
         global slot_temp2
         global flag
@@ -284,7 +359,7 @@ class project(QDialog):
             flag = 0
         print(items)
 
-    def on_pushButton2_clicked(self):
+    def on_pushButton2c_clicked(self):
         global slot_inputvalue1
         global slot_temp2
         global flag
@@ -336,74 +411,107 @@ class project(QDialog):
                         ':val1': str(slot_inputvalue1)
                         }
                     )
-            message_value = "Hello " + first_name + " Slot number: " + str(slot_inputvalue1) + " is booked at Dock number: 1"
             print(mobile_number)
-            # Send your sms message.
-            client.publish(
-                PhoneNumber = mobile_number,
-                Message = message_value
-            )
-            #self.accept()
-            global conf_flag
-            global repeat_flag
-            conf_flag = 1
-            repeat_flag = 1
+            # Send your sms message by publishing it to mqtt.
+            message_value = "Booking complete for" + first_name + " Slot value: " + str (slot_inputvalue1) + " and dock 1"
+            message = "{\"message_value\":\"" + message_value +"\",\"mobile_number\":\"" + mobile_number + "\"}"
+            myclient.publish("harry_policy_new",message, 0)
+
+            self.accept()
         else:
             QtWidgets.QMessageBox.warning(self,'Unavailable','Not Empty!')
             
-    def on_pushButton3_clicked(self):
-        global login_flag
-        global repeat_flag
-        login_flag = 1
-        repeat_flag = 1
-        #sys.exit(app.exec_())
+    def on_pushButton3c_clicked(self):
+        sys.exit(app.exec_())
 
+class unlock_slot(QDialog):
+    def __init__(self):
+        super(unlock_slot, self).__init__()
+        loadUi ('unlock.ui',self)
+        self.setWindowTitle('unlock window')
+        self.unlock_button.clicked.connect(self.on_unlock_buttonc_clicked)
+        self.timer_value.setValue(10) 
+
+    def on_unlock_buttonc_clicked(self):
+        timer_val = self.timer_value.value() 
+        global slot_value
+        global rfid_value
+        global continue_reading
+        TAGRead = MFRC522.MFRC522()
+        while continue_reading:
+            # Scan for cards   
+            time.sleep(1)
+            (status,TagType) = TAGRead.MFRC522_Request(TAGRead.PICC_REQIDL)
+            # If a card is found
+            if status == TAGRead.MI_OK:
+                print ("Card detected")
+    
+            # Get the UID of the card
+            (status,uid) = TAGRead.MFRC522_Anticoll()
+            # If we have the UID, continue
+            if status == TAGRead.MI_OK:
+                rfid_value = str(uid[0])+str(uid[1])+str(uid[2])+str(uid[3])
+                print(rfid_value)
+                continue_reading = False
+        table1 = dynamodb.Table('users')
+        response = table1.scan(
+                FilterExpression=Attr('RFID_value').eq(rfid_value)
+                )
+        items = response['Items']
+        for i in items:
+            if i['RFID_value'] != rfid_value:
+                QtWidgets.QMessageBox.warning(self,'Error','Invalid Entry')
+                sys.exit(app.exec_())       
+            elif i['slot'] == '0' and i['RFID_value'] == rfid_value:
+                QtWidgets.QMessageBox.warning(self,'Error','Slot not booked')
+                sys.exit(app.exec_())       
+                break
+            else:
+                print("Entered else loop")
+                slot_value = i['slot']
+                self.status_label.setText("Dock open for " + str(timer_val) + " sec")
+                sleep(timer_val)
+                self.status_label.setText("Closed")
+                self.accept()
 
 app = QApplication(sys.argv)
+Initial = initial_page()
+if Initial.exec_() == QtWidgets.QDialog.Accepted and username_login_flag == 1:
+    login = Login()
+    if login.exec_() == QtWidgets.QDialog.Accepted:
+        if test_flag == 1:
+            w = project()
+            w.show()
+            if w.exec_() == QtWidgets.QDialog.Accepted:
+                x=confirm()
+                x.show()
+                if x.exec_() == QtWidgets.QDialog.Accepted: 
+                    sys.exit(app.exec_())
+        elif test_flag == 0:
+            w = checkout()
+            w.show()
+            if w.exec_() == QtWidgets.QDialog.Accepted:
+                sys.exit(app.exec_())
+elif Initial.exec_() == QtWidgets.QDialog.Accepted and rfid_login_flag == 1:
+    login = Login_rfid()
+    if login.exec_() == QtWidgets.QDialog.Accepted:
+        if test_flag == 1:
+            w = project()
+            w.show()
+            if w.exec_() == QtWidgets.QDialog.Accepted:
+                x=confirm()
+                x.show()
+                if x.exec_() == QtWidgets.QDialog.Accepted: 
+                    sys.exit(app.exec_())
+        elif test_flag == 0:
+            w = checkout()
+            w.show()
+            if w.exec_() == QtWidgets.QDialog.Accepted:
+                sys.exit(app.exec_())
 
-conf = confirm()
-check = checkout()
-proj= project()
-login = Login()
-'''
-while 1:
-    if login_flag == 1 and repeat_flag == 1:
-        login.show()
-        proj.hide()
-        check.hide()
-        conf.hide()
-        login_flag = 0
-        repeat_flag = 0
-    elif proj_flag == 1 and repeat_flag == 1:
-        login.hide()
-        proj.show()
-        check.hide()
-        conf.hide()
-        proj_flag = 0
-        repeat_flag = 0
-    elif check_flag == 1 and repeat_flag == 1:
-        login.hide()
-        proj.hide()
-        check.show()
-        conf.hide()
-        check_flag = 0
-        repeat_flag = 0
-    elif conf_flag == 1 and repeat_flag == 1:
-        login.hide()
-        proj.hide()
-        check.hide()
-        conf.show()
-        conf_flag = 0
-        repeat_flag = 0
-    sleep(0.5)
-'''
-if login.exec_() == QtWidgets.QDialog.Accepted:
-    if test_flag == 1:
-        w = project()
-        w.show()
-    elif test_flag == 0:
-        w = checkout()
-        w.show()
-    if w.exec_() == QtWidgets.QDialog.Accepted:
-        x=confirm()
-        x.show()
+elif Initial.exec_() == QtWidgets.QDialog.Accepted:
+    login = unlock_slot()
+    if login.exec_() == QtWidgets.QDialog.Accepted:
+        sys.exit(app.exec_())
+
+
