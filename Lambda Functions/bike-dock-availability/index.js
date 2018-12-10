22b5c77c5b42e3ef6ff76efa20b5c3c0c91ec160
sharanjeetsinghmago <sharanjeet.mago@colorado.edu>
2018-12-10 02:25:11 -0700

    Update index.js

--- Lambda Functions/bike-dock-availability/index.js
+++ Lambda Functions/bike-dock-availability/index.js
@@ -11,36 +11,6 @@ exports.handler = (event, context, callback) => {
         TableName: "Availability"
     };
     
-    //  ddb.getItem(params, function(err, data) {
-    //   if (err) {
-    //     console.log("Error", err);
-
-    //   } else {
-    //       console.log("Booking status ----> ",data.Item.booking);
-          
-    //       var x = parse(data.Item.booking,String);
-          
-    //       console.log("x =",x);
-          
-    //       if(  data.Item.booking == "booked")
-    //       {
-    //           console.log("Dock is booked");
-    //           callback(null, "Dock is booked");
-    //       }
-    //       else if (x == "available")
-    //       {
-    //           console.log("Dock is available");
-    //           callback(null, "Dock is available");
-    //       }
-    //       else 
-    //       {
-    //           console.log("Ho rha hi nhi hai");
-    //       }
-    //     console.log("Success", data.Item);
-    //   // callback(null, JSON.parse( JSON.stringify(data, null, 2)));
-    //   }
-    // });
-
 
     ddb.scan(params, function(err, data) {
       if (err) {
@@ -51,20 +21,11 @@ exports.handler = (event, context, callback) => {
             var dock = parse(element.dock);
             var slot = parse(element.slot);
             var booking = parse(element.booking);
-          //console.log("Dock :",element.dock," Slot :",slot," Availability: ",booking);
-          //console.log(element.dock + " (" + element.slot + ")");
-          //callback(null,"Dock :");
           callback(null, JSON.parse( JSON.stringify(data, null, 2)));
         });
       }
     });
     
-    // ddb.listTables({Limit: 10}, function(err, data) {
-    //   if (err) {
-    //     console.log("Error", err.code);
-    //   } else {
-    //     console.log("Table names are ", data.TableNames);
-    //   }
-    // });
+    
 
 };
